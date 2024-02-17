4706a328942e0508b37ff07b14107cd41c07243f
Rudraksh Joshi <rudrakshjoshi27@gmail.com>
2024-02-17 23:04:39 +0530

    setting up multiple popups on different coordinates on click

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
+    // success handler
     function (position) {
       //   console.log(position);
       const { latitude } = position.coords;
@@ -25,16 +26,35 @@ if (navigator.geolocation) {
       // load map
       const map = L.map('map').setView(coords, 13);
 
+      // console.log(map);
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        // console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        const newCoords = [lat, lng];
+        // adding marker to the position where click happens
+
+        L.marker(newCoords)
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+            // can also use .setContent on pop()
+          )
+          .setPopupContent('hello world')
+          .openPopup();
+      });
     },
+    // failure handler
     function () {
       alert('Could not get your position');
     }
